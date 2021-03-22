a1eea57eb00acf0613749d3007bfb7c6bdeee18c
hassanelsayd <hassan22elsayd@gmail.com>
2021-03-22 02:59:15 +0200

    my project with bootstrap

---
+++ courses/web/html/html.js
@@ -0,0 +1,13 @@
+var code1 = document.getElementById("code1");
+code1.innerHTML = "<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>My First Heading</h1>
+<p>My first paragraph.</p>
+
+</body>
+</html>"
---
+++ js/main.js
@@ -0,0 +1,7 @@
+var z=document.getElementById("alogin");
+z.onclick=function (){
+   document.getElementById("plogin").innerHTML="please Sign In";
+   document.getElementById("sign-in").style.zIndex="1"
+   document.getElementById("sign-up").style.zIndex="0";
+ 
+}
